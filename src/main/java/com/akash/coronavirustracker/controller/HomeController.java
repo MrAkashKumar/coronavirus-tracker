b027aed5752a34bc4c33e14088e11eec96f0a725
Akashkumar4 <akash.kumar@oodlestechnologies.com>
2020-04-22 13:45:06 +0530

    refactor code

--- src/main/java/com/akash/coronavirustracker/controller/HomeController.java
+++ src/main/java/com/akash/coronavirustracker/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.akash.coronavirustracker.controller;
 
 import com.akash.coronavirustracker.DTO.RequestReport;
 import com.akash.coronavirustracker.service.CoronaVirusDataService;
+import com.akash.coronavirustracker.urlMapping.UrlMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,7 @@ public class HomeController {
     @Autowired
     private CoronaVirusDataService coronaVirusDataService;
 
-    @GetMapping("/")
+    @GetMapping(value = UrlMapping.GET_CONFIRMED_CASES)
     public String homeController(Model model){
         List<RequestReport> requestReports = coronaVirusDataService.getAllListReport();
         Integer totalReportedCases = requestReports.stream().mapToInt(stat ->stat.getLatestTotalCases()).sum();
--- src/main/java/com/akash/coronavirustracker/urlMapping/UrlMapping.java
+++ src/main/java/com/akash/coronavirustracker/urlMapping/UrlMapping.java
@@ -5,9 +5,11 @@ public class UrlMapping {
 
     public  UrlMapping(){
 
+
     }
 
 
     public static final String HOME_CONTROLLER = "/update";
+    public static final String GET_CONFIRMED_CASES = "/getConfirmedCases";
 
 }
